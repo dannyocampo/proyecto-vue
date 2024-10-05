4a6c6d76c03f8b5b2d3ae4214ed9b70c55b934f0
Danny Ocampo <tecnozone2012@gmail.com>
2024-10-04 19:25:54 -0500

    Update App and Main create archive Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>Hello World</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Incrementar</button>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  const contador = ref(0);
+  const incrementar = () => {
+    contador.value++;
+  }
+  const decrementar = () =>{
+    contador.value--;
+  }
+</script>
+
+<style scoped>
+  button{
+    margin: 10px 15px;
+  }
+</style>
