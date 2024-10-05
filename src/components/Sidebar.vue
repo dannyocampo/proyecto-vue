306303df046e8de2b4f9aa4fdb50a0af9758b17c
Danny Ocampo <tecnozone2012@gmail.com>
2024-10-04 21:27:13 -0500

    sideBar and mainmenu

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div class="sidebar">
+        <h2>Barra Lateral</h2>
+        <ul>
+            <li>
+                <RouterLink to="/contador">Contador</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/Lista-de-Tareas">Lista de Tareas</RouterLink>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    .sidebar{
+        width: 200px;
+        background: #f0f0f0;
+        padding: 20px;
+    }
+</style>
